29da8b1a84618ba2b737d6057af2491a0ef2c7ea
yoxia <171149510@qq.com>
2025-01-17 18:07:44 +0800

    todoList 基本功能开发

--- src/demo1/todo.cpp
+++ src/demo1/todo.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <windows.h>
 
-
 struct Task
 {
     std::string description;
@@ -12,10 +11,15 @@ struct Task
     Task(std::string desc, std::string due) : description(desc), dueDate(due), isCompleted(false) {}
 };
 
+void clearWindow();
+void showTasks(std::vector<Task> &tasks);
+
 void showMenu()
 {
     std::cout << "1. 添加任务" << std::endl;
     std::cout << "2. 查看任务" << std::endl;
+    std::cout << "3. 删除任务" << std::endl;
+    std::cout << "4. 完成任务" << std::endl;
     std::cout << "q. 退出" << std::endl;
 }
 
@@ -23,10 +27,63 @@ void addTask(std::vector<Task> &tasks, std::string desc, std::string due)
 {
     tasks.push_back(Task(desc, due));
 }
+void deleteTask(std::vector<Task> &tasks, int index)
+{
+    if (index > 0 && index <= tasks.size())
+    {
+        tasks.erase(tasks.begin() + index - 1);
+        std::cout << "任务已删除" << std::endl;
+    }
+    else
+    {
+        std::cout << "无效的编号" << std::endl;
+    }
+}
+
+void completeTask(std::vector<Task> &tasks, int index)
+{
+    if (index > 0 && index <= tasks.size())
+    {
+        if (tasks[index - 1].isCompleted)
+        {
+            std::cout << "当前任务是已完成的" << std::endl;
+        }
+        else
+        {
+            tasks[index - 1].isCompleted = true;
+            std::cout << "任务已完成" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "无效的编号" << std::endl;
+    }
+}
+void handleChoice(std::vector<Task> &tasks, char &choice, std::string type)
+{
+    clearWindow();
+    showTasks(tasks);
+    std::cout << "请输入要" << (type == "delete" ? "删除" : "完成") << "的任务编号: ";
+    int index;
+    std::cin >> index;
+    if (type == "delete")
+    {
+        deleteTask(tasks, index);
+    }
+    if (type == "complete")
+    {
+        completeTask(tasks, index);
+    }
+    std::cout << "输入r返回主菜单" << std::endl;
+    std::cin >> choice;
+    if (choice == 'r')
+    {
+        clearWindow();
+    }
+}
 void clearWindow()
 {
     std::cout << "\033[2J\033[1;1H";
-
 }
 void showTasks(std::vector<Task> &tasks)
 {
@@ -44,12 +101,14 @@ int main()
     tasks.push_back(Task("学习C++", "2025-01-17"));
     tasks.push_back(Task("学习Python", "2025-01-18"));
     char choice;
-    
+
     while (choice != 'q')
     {
         showMenu();
         std::cin >> choice;
-        if (choice == '1')
+        switch (choice)
+        {
+        case '1':
         {
             clearWindow();
             std::string desc, due;
@@ -60,18 +119,41 @@ int main()
             std::getline(std::cin, due);
             addTask(tasks, desc, due);
             clearWindow();
-            
+            break;
         }
-        if (choice == '2')
+        case '2':
         {
+
             clearWindow();
             showTasks(tasks);
-            std::cout<<"输入r返回主菜单"<<std::endl;
-            std::cin>>choice;
-            if(choice=='r')
+            std::cout << "输入r返回主菜单" << std::endl;
+            std::cin >> choice;
+            if (choice == 'r')
             {
                 clearWindow();
             }
+            break;
+        }
+        case '3':
+        {
+            handleChoice(tasks, choice, "delete");
+            break;
+        }
+        case '4':
+        {
+            handleChoice(tasks, choice, "complete");
+            break;
+        }
+        case 'q':
+        {
+            std::cout << "退出程序" << std::endl;
+            break;
+        }
+        default:
+        {
+            std::cout << "无效的输入" << std::endl;
+            break;
+        }
         }
     }
 
